a312f8001d278d6680a7dbfab62b8001c9494b55
Paul <157764547+Safaero@users.noreply.github.com>
2024-01-26 20:38:24 -0600

    actualizacion condicionales
    si

---
+++ condicionales/1st.py
@@ -0,0 +1,6 @@
+contraseñasav = "contraseña"
+contrasenausu = input("Introduce tu contraseña: ")
+if contraseñasav.lower() == contrasenausu.lower():
+    print("Contraseña correcta")
+else:
+    print("Contraseña incorrecta. Inténtalo de nuevo.")
