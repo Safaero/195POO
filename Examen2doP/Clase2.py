09c98595861407ba0d4a0dabad521710ec5abc59
Paul <157764547+Safaero@users.noreply.github.com>
2024-03-11 21:39:40 -0600

    MatriculasCor
    corrección

--- Examen2doP/Clase2.py
+++ Examen2doP/Clase2.py
@@ -1,12 +1,9 @@
 import random
 
 class GeneradorMatricula:
-    def __init__(self):
-        self.caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    def generar_matricula(self, nombre, apep, apem, nacimiento, curso, carrera):
+        
+        matricula = nombre[0] + apep[:2] + apem[:2] + nacimiento[-2:] + carrera[:3] + curso[-2:]
 
-    def generar_matricula(self, longitud, especialesCar):
-        if especialesCar:
-            self.caracteres += "!@#$%^&*()_+-=[]{}|;:,.<>?`~"
-
-        contraseña = ''.join(random.sample(self.caracteres, longitud))
-        return contraseña
+        matricula += ''.join(random.choices("0123456789", k=2))
+        return matricula
--- Examen2doP/interf2.py
+++ Examen2doP/interf2.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
 from Clase2 import GeneradorMatricula
+import random
+
 class InterfazGenerador:
-    
-    
     def __init__(self, ventana):
         self.ventana = ventana
         self.ventana.title("Examen 2 :)")
@@ -16,25 +16,22 @@ class InterfazGenerador:
         self.carrer_var = tk.StringVar(value='')
 
         tk.Label(self.ventana, text="Nombre:").pack()
-        self.entry_longitud = tk.Entry(self.ventana, textvariable=self.nombre_var)
-        self.entry_longitud.pack()
+        tk.Entry(self.ventana, textvariable=self.nombre_var).pack()
         
         tk.Label(self.ventana, text="Apellido paterno").pack()
-        self.entry_longitud = tk.Entry(self.ventana, textvariable=self.apep_var)
-        self.entry_longitud.pack()
+        tk.Entry(self.ventana, textvariable=self.apep_var).pack()
         
         tk.Label(self.ventana, text="Apellido materno").pack()
-        self.entry_longitud = tk.Entry(self.ventana, textvariable=self.apem_var)
-        self.entry_longitud.pack()
+        tk.Entry(self.ventana, textvariable=self.apem_var).pack()
         
         tk.Label(self.ventana, text="Año de nacimiento").pack()
-        self.entry_longitud = tk.Entry(self.ventana, textvariable=self.nac_var)
-        self.entry_longitud.pack()
+        tk.Entry(self.ventana, textvariable=self.nac_var).pack()
         
-        tk.Label(self.ventana, text="carrera:").pack()
-        self.entry_longitud = tk.Entry(self.ventana, textvariable=self.carrer_var)
-        self.entry_longitud.pack()
-
+        tk.Label(self.ventana, text="Curso:").pack()
+        tk.Entry(self.ventana, textvariable=self.curs_var).pack()
+        
+        tk.Label(self.ventana, text="Carrera:").pack()
+        tk.Entry(self.ventana, textvariable=self.carrer_var).pack()
 
         self.boton_generar = tk.Button(self.ventana, text="Generar Matricula", command=self.generar_matricula)
         self.boton_generar.pack()
@@ -43,7 +40,6 @@ class InterfazGenerador:
         self.entry_contraseña = tk.Entry(self.ventana)
         self.entry_contraseña.pack()
 
-
     def generar_matricula(self):
         Nombre = self.nombre_var.get()
         Apep = self.apep_var.get()
@@ -51,14 +47,13 @@ class InterfazGenerador:
         Nacimiento = self.nac_var.get()
         Curso = self.curs_var.get()
         Carrera = self.carrer_var.get()
-        Random = self.rando_var.get()
 
         generador = GeneradorMatricula()
-        contraseña = generador.generar_matricula(Nombre, Apep, Apem, Nacimiento, Curso, Carrera, Random)
+        matricula = generador.generar_matricula(Nombre, Apep, Apem, Nacimiento, Curso, Carrera)
 
         self.entry_contraseña.delete(0, tk.END)
-        self.entry_contraseña.insert(0, contraseña)
-        
+        self.entry_contraseña.insert(0, matricula)
+
 ventana = tk.Tk()
 app = InterfazGenerador(ventana)
 ventana.mainloop()
